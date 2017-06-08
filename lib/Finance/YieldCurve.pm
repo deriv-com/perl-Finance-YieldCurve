68898506e9acc559d713dd79587685c8a7e1f0b9
Tom Molesworth <tom@binary.com>
2017-06-08 10:18:15 +0800

    Updated to provide two methods for interpolation

--- lib/Finance/YieldCurve.pm
+++ lib/Finance/YieldCurve.pm
@@ -8,61 +8,92 @@ our $VERSION = '0.001';
 
 =head1 NAME
 
-Finance::YieldCurve
+Finance::YieldCurve - provides methods for interpolation on interest rates or dividends
 
 =head1 SYNOPSIS
 
  use Finance::YieldCurve;
 
- my $interest_rates = Finance::YieldCurve->new(
+ my $rates = Finance::YieldCurve->new(
   data => {
    1  => 0.014,
    7  => 0.011,
    14 => 0.012,
   },
-  # Either we pass a type:
-  # type => 'dividend|yield',
-  # or an interpolation:
-  # type => 'closest|linear',
  );
- # or we select based on the method:
+ # For dividends, we return the closest value with no interpolation
  my $rate = $interest_rates->find_closest_to(7 * 24 * 60 * 60);
+ # For interest rates, we interpolate linearly between the points
  my $rate = $interest_rates->interpolate(7 * 24 * 60 * 60);
-
- my $dividends = Finance::YieldCurve->new(
-  data => {
-   1  => 0.025,
-   7  => 0.025,
-   14 => 0.028,
-  }
- );
- my $rate = $dividends->rate_for(7 * 24 * 60 * 60);
 
 =head1 DESCRIPTION
 
+
+
 =cut
 
+use Moo;
 
 use Math::Function::Interpolator;
 
-=head2 rate_for
+has data => (
+    is => 'ro'
+);
+
+=head2 interpolate
 
 Get the interpolated rate for this yield curve over the given time period (fractional years).
 
 Example:
 
- my $rate = $curve->rate_for(7 * 24 * 60 * 60);
+ my $rate = $curve->interpolate(7 * 24 * 60 * 60);
 
 =cut
 
-sub rate_for {
+sub interpolate {
     my ($self, $tiy) = @_;
 
     # timeinyears cannot be undef
     $tiy ||= 0;
 
-    my $interp = Math::Function::Interpolator->new(points => $self->rates);
+    my $interp = Math::Function::Interpolator->new(points => $self->data);
     return $interp->linear($tiy * 365) / 100;
+}
+
+=head2 find_closest_to
+
+Returns the closest point to the request value.
+
+Example:
+
+ my $rate = $curve->find_closest_to(7 * 24 * 60 * 60);
+
+=cut
+
+sub find_closest_to {
+    my ($self, $tiy) = @_;
+
+    # If we have undef or zero, bail out early
+    return 0 unless $tiy;
+
+    # Handle discrete dividend
+    my ($nearest_yield_days_before, $nearest_yield_before) = (0, 0);
+    my $days_to_expiry = $tiy * 365.0;
+    my $rates = $self->data;
+    my @sorted_expiries = sort { $a <=> $b } keys(%$rates);
+    foreach my $day (@sorted_expiries) {
+        if ($day <= $days_to_expiry) {
+            $nearest_yield_days_before = $day;
+            $nearest_yield_before      = $rates->{$day};
+            next;
+        }
+        last;
+    }
+
+    # Re-annualize
+    my $discrete_points = $nearest_yield_before * $nearest_yield_days_before / 365;
+
+    return $discrete_points * 365 / ($days_to_expiry * 100);
 }
 
 1;
